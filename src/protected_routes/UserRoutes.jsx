accf1ce484f0bfcc76dc2078a31c4d5e75330ae1
slesha45 <dahalslesha@gmail.com>
2025-02-01 23:27:16 +0545

    route

--- src/protected_routes/UserRoutes.jsx
+++ src/protected_routes/UserRoutes.jsx
@@ -6,11 +6,6 @@ const UserRoutes = () => {
     //get user information
     const user = JSON.parse(localStorage.getItem('user'))
 
-    //check user 
-    //check isAdmin = true
-    //if true : Acccess all the route of Admin (outlet)
-    //if false : Navigate to login
-
     return user != null ? <Outlet />
         : <Navigate to={'/login'} />
 }
